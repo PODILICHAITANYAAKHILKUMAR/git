9d8fcfcd7a4a359577e09fc9ba4dbf0d0dda9acc
PODILICHAITANYAAKHILKUMAR <pchakhilkumar1082001@gmail.com>
2023-09-30 21:21:29 +0530

    errors

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="modal">
+    <!-- Modal content -->
+    <multiselect
+      v-model="selectedOptions"
+      :options="options"
+      label="name"
+      track-by="id"
+      :multiple="true"
+    ></multiselect>
+  </div>
 </template>
 
+<script>
+import Multiselect from 'vue-multiselect';
+
+export default {
+  components: {
+    Multiselect,
+  },
+  data() {
+    return {
+      selectedOptions: [],
+      options: [
+        // Your options here
+      ],
+    };
+  },
+  // Other modal logic
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+/* Modal styles */
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  z-index: 1000;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+/* Override vue-multiselect styles */
+.multiselect {
+  position: fixed; /* Position dropdown outside modal */
+  top: 0;
+  left: 0;
+  z-index: 2000; /* Ensure it's above the modal */
 }
 </style>
